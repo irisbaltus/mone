08968ab9159ce40de1a89161d8af200566bce2ec
irisbaltus <150801536+irisbaltus@users.noreply.github.com>
2026-07-16 14:46:09 +0200

    slider bckrgnd change

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-console.log('hi')
+// console.log('hi')
+// const button = document.getElementById("toggle-btn");
+// const slider = document.querySelector(".slide-menu");
+// const closeSlider = document.getElementById("close-slider");
+
+// button.addEventListener("click", () => {
+//   slider.style.display = "flex";
+// });
+
+// closeSlider.addEventListener("click", () => {
+//   slider.style.display = "none";
+// });
+
+
+//slider
 const button = document.getElementById("toggle-btn");
 const slider = document.querySelector(".slide-menu");
 const closeSlider = document.getElementById("close-slider");
 
 button.addEventListener("click", () => {
-  slider.style.display = "flex";
+  slider.classList.add("open");
 });
 
 closeSlider.addEventListener("click", () => {
-  slider.style.display = "none";
+  slider.classList.remove("open");
 });
 
+//background
+const overlay = document.querySelector(".overlay");
+
+button.addEventListener("click", () => {
+    slider.classList.add("open");
+    overlay.classList.add("open");
+});
+
+closeSlider.addEventListener("click", () => {
+    slider.classList.remove("open");
+    overlay.classList.remove("open");
+});
